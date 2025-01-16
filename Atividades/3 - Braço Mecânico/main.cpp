3b0519d1978497b3cbdaf9159dc39c6ce63d74bd
BrunoViola <brunohenriquesilvaviola@gmail.com>
2025-01-15 22:18:14 -0300

    add: atividade de construção do braço mecânico

---
+++ Atividades/3 - Braço Mecânico/main.cpp
@@ -0,0 +1,151 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <GLFW/glfw3.h>
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+        switch (key) {
+            case GLFW_KEY_W:
+                glScalef(2, 1, 1);
+                break;
+            case GLFW_KEY_S:
+                glScalef(0.5, 1, 1);
+                break;
+            case GLFW_KEY_A:
+                glRotated(45, 0, 0, 1);
+                break;
+            case GLFW_KEY_D:
+                glRotated(-45, 0, 0, 1);
+                break;
+            case GLFW_KEY_RIGHT:
+                glTranslatef(10, 0, 0);
+                break;
+            case GLFW_KEY_LEFT:
+                glTranslatef(-10, 0, 0);
+                break;
+            case GLFW_KEY_UP:
+                glTranslatef(0, 10, 0);
+                break;
+            case GLFW_KEY_DOWN:
+                glTranslatef(0, -10, 0);
+                break;
+            case GLFW_KEY_ESCAPE:
+                glfwSetWindowShouldClose(window, true);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void desenhaBase(){
+    glBegin(GL_QUAD_STRIP);
+        glColor3f(0.678,0.847,0.902);
+        glVertex2f(260, 0); 
+        glVertex2f(360, 0);
+
+        glVertex2f(260, 90);
+        glVertex2f(360, 90);
+    glEnd();
+}
+
+void desenhaInferior(){
+    glBegin(GL_QUAD_STRIP);
+        glColor3f(0.978, 0.947,0.902);
+        glVertex2f(290, 110); 
+        glVertex2f(330, 110);
+
+        glVertex2f(290, 210);
+        glVertex2f(330, 210);
+    glEnd();
+}
+
+void desenhaSuperior(){
+    glBegin(GL_QUAD_STRIP);
+        glColor3f(0.378,0.747,0.402);
+        glVertex2f(300, 230); 
+        glVertex2f(320, 230);
+
+        glVertex2f(300, 330);
+        glVertex2f(320, 330);
+    glEnd();
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+    float x, y;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* O NULL passado como parâmetros indica que a janela não terá uma janela
+    de compartilhamento de contexto e que não é criada com um monitor específico. */
+    window = glfwCreateWindow(1240, 1480, "Atividade 3 - Braço Mecânico", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Definição do Contexto Atual:
+    Torna o contexto OpenGL da janela criada como sendo o contexto atual.
+    Quer dizer que qualquer chamada seguinte ao OpenGL irá afetar esta janela. */
+    glfwMakeContextCurrent(window);
+
+    /* Configura o viewport e a projeção ortográfica */
+    glViewport(0, 0, 1240, 1480);               // Ajusta o viewport para o tamanho da janela
+    glMatrixMode(GL_PROJECTION);              // Muda para a matriz de projeção
+    glLoadIdentity();                         // Reseta a matriz de projeção para identidade
+    glOrtho(0, 640, 0, 480, -1, 1);           // Define uma projeção ortográfica 2D
+
+    /* Define a função de callback para o teclado */ 
+    glfwSetKeyCallback(window, key_callback);
+
+    /* Loop de execução da janela enquanto não for fechada */
+    while (!glfwWindowShouldClose(window))
+    {
+        // Define a cor de fundo
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        desenhaBase();
+
+        glTranslatef(310, 90, 0);
+        glRotatef(sin(glfwGetTime()) * 45, 0, 0, 1);
+        glTranslatef(-310, -90, 0);
+        desenhaInferior();
+
+        glTranslatef(310, 210, 0);
+        glRotatef(sin(glfwGetTime()) * 50, 0, 0, 1);
+        glTranslatef(-310, -210, 0);
+        desenhaSuperior();
+
+        #ifdef _WIN32
+        Sleep(50);
+        #else
+        usleep(50);
+        #endif
+        /* Troca os buffers (Mas veremos mais adiante a importância desta parte) */
+        glfwSwapBuffers(window);
+
+        /* Chamada para verificar e processar todos os eventos que ocorreram,
+        como entradas do teclado, mouse, ... Isso mantém a janela responsiva. */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
