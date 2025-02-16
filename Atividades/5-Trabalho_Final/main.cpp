3e826ba59a44597b9f2232dee25fd5cf86cca24c
BrunoViola <brunohenriquesilvaviola@gmail.com>
2025-02-16 16:50:10 -0300

    cria um .c e .h para desenho de arquibancadas [Trab. Final]

--- Atividades/5-Trabalho_Final/main.cpp
+++ Atividades/5-Trabalho_Final/main.cpp
@@ -3,6 +3,7 @@
 #include <GL/glut.h>
 #include <stdio.h>
 #include <math.h>
+#include "arquibancada.h"
 
 float ballY = 2.0f;
 float ballX = 0.0f;
@@ -198,8 +199,6 @@ void desenhaBoneco() {
 }
 // ======================================================================
 
-
-
 // ======================== Funções de desenho ========================
 // Função para desenhar a bola
 void desenhaBola() {
@@ -291,70 +290,6 @@ void desenhaParedeGol(){
     glPushMatrix();
     glTranslatef(0.0f, 2.5f, -6.0f);
     glScalef(4.0f, 0.5f, 1.0f);
-    glutSolidCube(1.0);
-    glPopMatrix();
-}
-
-void desenhaArquibancadaDireita(){
-    glPushMatrix(); //primeiro degrau
-    glDisable(GL_LIGHTING);
-    glColor3f(0.1f, 0.1f, 0.1f);
-    glTranslatef(5.0f, -1.0f, 0.5f);
-    glScalef(1.0f, 1.0f, 14.0f);
-    glutSolidCube(1.0);
-    glEnable(GL_LIGHTING);
-    glPopMatrix();
-
-    glPushMatrix(); //segundo degrau
-    glTranslatef(6.0f, 0.0f, 1.5f);
-    glScalef(1.0f, 1.0f, 16.0f);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-    glPushMatrix(); //terceiro degrau
-    glDisable(GL_LIGHTING);
-    glColor3f(0.1f, 0.1f, 0.1f);
-    glTranslatef(7.0f, 1.0f, 1.5f);
-    glScalef(1.0f, 1.0f, 16.0f);
-    glutSolidCube(1.0);
-    glEnable(GL_LIGHTING);
-    glPopMatrix();
-    
-    glPushMatrix(); //quarto degrau
-    glTranslatef(8.0f, 2.0f, 1.5f);
-    glScalef(1.0f, 1.0f, 16.0f);
-    glutSolidCube(1.0);
-    glPopMatrix();
-}
-
-void desenhaArquibancadaFrente(){
-    glPushMatrix(); //primeiro degrau
-    glDisable(GL_LIGHTING);
-    glColor3f(0.1f, 0.1f, 0.1f);
-    glTranslatef(0.5f, -1.0f, 7.0f);
-    glScalef(11.0f, 1.0f, 1.0f);
-    glutSolidCube(1.0);
-    glEnable(GL_LIGHTING);
-    glPopMatrix();
-    
-    glPushMatrix(); //segundo degrau
-    glTranslatef(1.0f, 0.0f, 8.0f);
-    glScalef(12.0f, 1.0f, 1.0f);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-    glPushMatrix(); //terceiro degrau
-    glDisable(GL_LIGHTING);
-    glColor3f(0.1f, 0.1f, 0.1f);
-    glTranslatef(1.75f, 1.0f, 9.0f);
-    glScalef(13.5f, 1.0f, 1.0f);
-    glutSolidCube(1.0);
-    glEnable(GL_LIGHTING);
-    glPopMatrix();
-
-    glPushMatrix(); //quarto degrau
-    glTranslatef(1.75f, 2.0f, 10.0f);
-    glScalef(13.5f, 1.0f, 1.0f);
     glutSolidCube(1.0);
     glPopMatrix();
 }
